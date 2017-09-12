0b9325968711b79c8f17af2cea0af2e9b0ffb32b
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-12 22:01:22 +0800

    add indentation with example of absolute value

---
+++ src/Lesson2_Indentation.py
@@ -0,0 +1,8 @@
+#python 由缩进控制格式，不需要 ； {}
+#valueA = input("Please input the value:")
+valueA = 100
+if valueA >= 0:
+    print("The absolute value of",valueA,"is",valueA)
+else:
+    print("The absolute value of",valueA,"is",-valueA)
+    
