28916d3bd39d9e86dee86553bf1c160779ab102c
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-15 10:19:34 +0800

    add set

--- src/Lesson8_Dict&Set.py
+++ src/Lesson8_Dict&Set.py
@@ -3,6 +3,7 @@
 # dict
 # {}
 # 字典是一种 Key-Value/Item 模型 ，通过 Key 来查询 Value 的值 ，Key不能相同，Value的值可以相同
+# Key一定是一种 不可变对象 ，
 # 
 # dict 的建立
 # dictExample = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
@@ -32,4 +33,27 @@
 # dictExample.pop( 'Deng'[,100])
 # 返回值 100
 # [,100] 可以省略，返回值是 Key 对应的 Item
+# #
+
+# #
+# 集合
+# set
+# 一系列 不重复 且 无序 的元素集合
+# 与 dict 类似，但是只存储 Key ，不存储 Item/Value
+# 定义 ： set() 方法
+# setExample = set([1,2,3])
+# 可以定义一个空的 set ，也可以用 list 来定义/新建一个 set
+# 
+# 增加元素 
+# 增加的元素没有顺序，因为本身也是无序的
+# setExample.add(4)
+# 
+# 删除元素
+# setExample.delete(4)
+# 
+# 因为 set 可以看成数学上的集合，所以还会有集合的操作
+# 
+# 交集 s and s1
+# 
+# 并集 s or s1
 # #
