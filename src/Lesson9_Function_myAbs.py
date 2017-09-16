ea06d2011b776b35d857f563c2c221f07aafd6e6
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-16 16:27:51 +0800

    add type check

--- src/Lesson9_Function_myAbs.py
+++ src/Lesson9_Function_myAbs.py
@@ -1,5 +1,14 @@
+# def myAbs(x) :
+#     if x <= 0:
+#         return -x
+#     else:
+#         return x
+
+# 带有类型检查
 def myAbs(x) :
+    if not isinstance(x,(int,float)):
+        raise TypeError('bad operand type')
     if x <= 0:
-        return x
-    else:
         return -x
+    else:
+        return x
