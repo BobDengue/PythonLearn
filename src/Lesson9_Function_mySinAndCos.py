13a70d34a171b4261cb95b5c65e55b5b5978f7d9
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-16 17:55:05 +0800

    add function sinandcos

---
+++ src/Lesson9_Function_mySinAndCos.py
@@ -0,0 +1,6 @@
+#This is a program solve the sin and cos value for angle
+import math
+def sinAndCos(angle):
+    angle_sin = math.sin( angle)
+    angle_cos = math.cos( angle)
+    return angle_sin,angle_cos
