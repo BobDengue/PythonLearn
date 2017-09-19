9abb65cab5e8ac0ed19f0410431f72feb43ddd72
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-19 20:15:34 +0800

    add generator triangles( yanghui sanjiao)

--- src/Lesson13_Generator_Triangles.py
+++ src/Lesson13_Generator_Triangles.py
@@ -1,8 +1,16 @@
-def Triangles( degreeOfAngle=6) :
-    counter = 1
-    angleList = [1]      
-    if counter == degreeOfAngle :
-        return
-    angleList.
-    counter += 1 
-    yield 
+# def Triangles( degreeOfAngle=6) :
+#     counter = 1
+#     angleList = [1]      
+#     if counter == degreeOfAngle :
+#         return
+#     angleList.
+#     counter += 1 
+#     yield 
+
+# from baidu baike
+def Triangles() :
+    L = [1] 
+    while True :
+        yield L
+        L.append(0)
+        L=[ L[i-1] + L[i] for i in range(len(L))]
