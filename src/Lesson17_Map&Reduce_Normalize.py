c70a4a1d35d99bbdc5d8ff5153a2c14f1378aff7
Deng Baoyuan <deng1063284500@gmail.com>
2017-09-21 23:02:55 +0800

    add normal not complished

---
+++ src/Lesson17_Map&Reduce_Normalize.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+# 把输入的 str 标准化
+# 也就是首字母大写，其他小写
+
+from functools 
+
+def Normalize(*name):
+    for n in name :
+        return 
+
+
+# 求 积
+def prod(L) :
+
+
+# 字符串到浮点
+def str2float(s):
